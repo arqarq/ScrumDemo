040f7ab3fcf7c177f41bae5213668e8d742f1820
Pawel Bialas <p.cz.bialas@gmail.com>
2019-03-09 14:52:30 +0100

    service added

---
+++ ScrumDemo/src/main/java/pl/sdacacademy/scrum/ScrumDemo/model/ContactService.java
@@ -0,0 +1,13 @@
+package pl.sdacacademy.scrum.ScrumDemo.model;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository repository;
+
+}
