332a988eb13dc8a65d6313e7f58b9f3ede6a714f
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-10 09:58:48 +0100

    - application.properties

--- src/main/java/pl/sdacacademy/scrumdemo/repositories/ContactRepository.java
+++ src/main/java/pl/sdacacademy/scrumdemo/repositories/ContactRepository.java
@@ -1,6 +1,7 @@
-package pl.sdacacademy.scrumdemo.model;
+package pl.sdacacademy.scrumdemo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import pl.sdacacademy.scrumdemo.model.Contact;
 
 public interface ContactRepository extends CrudRepository<Contact, Long> {
 }
--- src/main/java/pl/sdacacademy/scrumdemo/service/ContactService.java
+++ src/main/java/pl/sdacacademy/scrumdemo/service/ContactService.java
@@ -3,7 +3,7 @@ package pl.sdacacademy.scrumdemo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.sdacacademy.scrumdemo.model.Contact;
-import pl.sdacacademy.scrumdemo.model.ContactRepository;
+import pl.sdacacademy.scrumdemo.repositories.ContactRepository;
 
 @Service
 public class ContactService {
