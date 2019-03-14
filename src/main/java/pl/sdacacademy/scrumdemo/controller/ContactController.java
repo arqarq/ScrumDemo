4315b2b686a1f0a485cad820e9637389a2a14dc7
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-15 00:11:09 +0100

    - GETting all and POSTing one/many implemented

--- src/main/java/pl/sdacacademy/scrumdemo/controller/ContactController.java
+++ src/main/java/pl/sdacacademy/scrumdemo/controller/ContactController.java
@@ -3,11 +3,11 @@ package pl.sdacacademy.scrumdemo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import pl.sdacacademy.scrumdemo.model.Contact;
 import pl.sdacacademy.scrumdemo.service.ContactService;
+
+import java.util.List;
 
 @Controller
 public class ContactController {
@@ -18,9 +18,20 @@ public class ContactController {
         this.contactService = contactService;
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody
+    List<Contact> getContacts() {
+        return contactService.getAll();
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public void addContact(@RequestBody Contact contact) {
-        contactService.save(contact);
+    public void addContact(@RequestBody List<Contact> contacts) {
+        if (contacts != null) {
+            for (Contact temp : contacts) {
+                contactService.save(temp);
+            }
+        }
     }
 }
--- src/main/java/pl/sdacacademy/scrumdemo/service/ContactService.java
+++ src/main/java/pl/sdacacademy/scrumdemo/service/ContactService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import pl.sdacacademy.scrumdemo.model.Contact;
 import pl.sdacacademy.scrumdemo.repositories.ContactRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ContactService {
     private final ContactRepository contactRepository;
@@ -12,6 +15,15 @@ public class ContactService {
     @Autowired
     ContactService(ContactRepository contactRepository) {
         this.contactRepository = contactRepository;
+    }
+
+    public List<Contact> getAll() {
+        List<Contact> list = new ArrayList<>();
+        Iterable<Contact> allObj = contactRepository.findAll();
+        if (allObj != null) {
+            list = (List<Contact>) allObj;
+        }
+        return list;
     }
 
     public void save(Contact contact) {
