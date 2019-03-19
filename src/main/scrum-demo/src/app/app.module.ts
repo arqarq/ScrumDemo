89661080effcf94a32db7af4261c08b5f65e4d4c
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-20 00:28:56 +0100

    - css

--- src/main/scrum-demo/src/app/app.module.ts
+++ src/main/scrum-demo/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ListAllComponent } from './list-all/list-all.component';
+import {AppComponent} from './app.component';
+import {ListAllComponent} from './list-all/list-all.component';
+import {FormsModule} from "@angular/forms";
+import {NgSelectModule} from "@ng-select/ng-select";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { ListAllComponent } from './list-all/list-all.component';
     ListAllComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/main/scrum-demo/src/app/list-all/list-all.component.ts
+++ src/main/scrum-demo/src/app/list-all/list-all.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgSelectConfig} from "@ng-select/ng-select";
 
 @Component({
   selector: 'app-list-all',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-all.component.css']
 })
 export class ListAllComponent implements OnInit {
+  bullets = [
+    {name: 1, value: "Arek"},
+    {name: 2, value: "Sekuła"}
+  ];
 
-  constructor() { }
+  cities = [
+    {id: 1, name: 'Vilnius', avatar: '//www.gravatar.com/avatar/b0d8c6e5ea589e6fc3d3e08afb1873bb?d=retro&r=g&s=30 2x'},
+    {id: 2, name: 'Kaunas', avatar: '//www.gravatar.com/avatar/ddac2aa63ce82315b513be9dc93336e5?d=retro&r=g&s=15'},
+    {id: 3, name: 'Pavilnys', avatar: '//www.gravatar.com/avatar/6acb7abf486516ab7fb0a6efa372042b?d=retro&r=g&s=15'},
+    {id: 4, name: 'Siauliai', avatar: '//www.gravatar.com/avatar/b0d8c6e5ea589e6fc3d3e08afb1873bb?d=retro&r=g&s=30 2x'},
+  ];
+
+  cities3 = this.cities.slice();
+  selectedCity = this.cities[0].name;
+  selectedCity3 = this.cities3[2].name;
+
+  constructor(private config: NgSelectConfig) {
+    this.config.notFoundText = 'Custom not found';
+  }
 
   ngOnInit() {
   }
-
 }
