40459001641afa38f98b664ea5888ca50f0a1e89
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-23 19:38:21 +0100

    - setting title

--- scrum-demo/src/app/app.component.ts
+++ scrum-demo/src/app/app.component.ts
@@ -1,14 +1,20 @@
-import {Component} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public constructor(private titleService: Title) {
+  }
 
-  title = 'scrum-demo';
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle)
+  }
 
-
+  ngOnInit() {
+    this.setTitle('Twitter')
+  }
 }
--- scrum-demo/src/app/app.module.ts
+++ scrum-demo/src/app/app.module.ts
@@ -1,26 +1,9 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserModule, Title} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {ListAllComponent} from './list-all/list-all.component';
 import {FormsModule} from "@angular/forms";
 import {NgSelectModule} from "@ng-select/ng-select";
-import {RouterModule, Routes} from '@angular/router';
-
-const appRoutes: Routes = [
-  {path: 'dodajuzytkownika', component: null},
-  {path: 'hero/:id', component: null},
-  {
-    path: 'heroes',
-    component: null,
-    data: {title: 'Heroes List'}
-  },
-  {
-    path: '',
-    redirectTo: '/heroes',
-    pathMatch: 'full'
-  },
-  {path: '**', component: null}
-];
 
 @NgModule({
   declarations: [
@@ -31,13 +14,8 @@ const appRoutes: Routes = [
     BrowserModule,
     NgSelectModule,
     FormsModule
-    // RouterModule.forRoot(
-    //   appRoutes,
-    //   {enableTracing: true} // <-- debugging purposes only
-    // )
-    // other imports here
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule {
