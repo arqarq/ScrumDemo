6928fcf3b187d96566b3cdbb8b85cf03fe7015c1
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-25 01:42:16 +0100

    - new-account component

--- scrum-demo/src/app/new-account/new-account.component.ts
+++ scrum-demo/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-new-account',
@@ -6,10 +7,18 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent implements OnInit {
-
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  submitHide(f: NgForm): boolean {
+    return !(f.controls['login'].dirty && f.controls['login'].valid) ||
+      (f.controls['email'].invalid);
+  }
+
+  save(f: NgForm) {
+    alert("OK!")
   }
 }
